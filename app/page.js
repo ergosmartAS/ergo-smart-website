96cd382b403f37cd2a09478481bd9df06e563fc7
Jorgen <sjkodehode2025@gmail.com>
2025-05-04 12:32:09 +0200

    added and fixed some styling

--- app/page.js
+++ app/page.js
@@ -13,11 +13,20 @@ export default function Home() {
     <main>
         <Header />     
         <HomeSection />
-        <BentoGrid />
-          <HealthSection />
+        <BentoGrid /> 
+        <HealthSection />
         <ProductShowcase />
         <AboutUs />
-        
+        <div className="bg-[#e8f6cd]">
+          <div className="pt-40 " style={{ maxWidth: '1240px', overflow: 'hidden', margin: 'auto' }}>
+            <h2 className="text-center text-6xl mb-20 font-bold">Brosjyre</h2>
+            <iframe
+              src="https://gamma.app/embed/ckjt4kg5brv4u80"
+              style={{ width: 'calc(100% + 17px)', height: '1000px', border: 'none' }}
+              title="Embedded Content"
+              ></iframe>
+          </div>
+        </div>
         <OurProducts />
         <Footer />
     </main>
